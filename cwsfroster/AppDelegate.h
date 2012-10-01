b4c42ae3fad476baade63d15d5fe58dfac64332b
Bobby Ren <bobbyren@gmail.com>
2012-09-30 22:58:28 -0700

    Initial Commit

---
+++ cwsfroster/AppDelegate.h
@@ -0,0 +1,15 @@
+//
+//  AppDelegate.h
+//  cwsfroster
+//
+//  Created by Bobby Ren on 9/30/12.
+//  Copyright (c) 2012 Bobby Ren. All rights reserved.
+//
+
+#import <UIKit/UIKit.h>
+
+@interface AppDelegate : UIResponder <UIApplicationDelegate>
+
+@property (strong, nonatomic) UIWindow *window;
+
+@end
