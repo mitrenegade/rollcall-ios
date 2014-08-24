92fc2f20ef279e1a77b5866344a6aa4748d1ed2d
mitrenegade <bobbyren@gmail.com>
2014-08-24 15:49:32 -0700

    practices are modal

--- cwsfroster/AttendancesViewController.m
+++ cwsfroster/AttendancesViewController.m
@@ -67,6 +67,9 @@
 }
 
 #pragma mark - Navigation
+-(IBAction)didClickClose:(id)sender {
+    [self.navigationController dismissViewControllerAnimated:YES completion:nil];
+}
 
 // In a storyboard-based application, you will often want to do a little preparation before navigation
 - (void)prepareForSegue:(UIStoryboardSegue *)segue sender:(id)sender
