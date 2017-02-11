f209e98bdd4664f9606cccc579a1458bc9bc9f4a
Bobby <bobbyren@gmail.com>
2017-02-11 08:37:49 -0500

    added row to go to onsite signup from attendanceViewController

--- cwsfroster/AttendanceTableViewController.swift
+++ cwsfroster/AttendanceTableViewController.swift
@@ -55,25 +55,34 @@ class AttendanceTableViewController: UITableViewController {
 // MARK: - Table view data source
 extension AttendanceTableViewController {
     override func numberOfSections(in tableView: UITableView) -> Int {
-        return 1
+        return 2
     }
 
     override func tableView(_ tableView: UITableView, numberOfRowsInSection section: Int) -> Int {
 //        guard let practice = self.practice else { return 0 }
 //        return practice.attendances?.count ?? 0
+        if section == 0 {
+            return 1
+        }
         guard let members = Organization.current?.members else { return 0 }
         return members.count
     }
 
     override func tableView(_ tableView: UITableView, cellForRowAt indexPath: IndexPath) -> UITableViewCell {
-        let cell = tableView.dequeueReusableCell(withIdentifier: "AttendanceCell", for: indexPath)
-
-        guard let attendanceCell = cell as? AttendanceCell else { return cell }
-        // Configure the cell...
-        guard let members = Organization.current?.members, indexPath.row < members.count else { return cell }
-        let member = members[indexPath.row]
-        attendanceCell.configure(member: member, practice: self.practice, newAttendance: newAttendances[member], row: indexPath.row)
-        return cell
+        if indexPath.section == 0 {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "OnSiteSignupCell", for: indexPath)
+            return cell
+        }
+        else {
+            let cell = tableView.dequeueReusableCell(withIdentifier: "AttendanceCell", for: indexPath)
+            
+            guard let attendanceCell = cell as? AttendanceCell else { return cell }
+            // Configure the cell...
+            guard let members = Organization.current?.members, indexPath.row < members.count else { return cell }
+            let member = members[indexPath.row]
+            attendanceCell.configure(member: member, practice: self.practice, newAttendance: newAttendances[member], row: indexPath.row)
+            return cell
+        }
     }
 }
 
@@ -81,6 +90,11 @@ extension AttendanceTableViewController {
 extension AttendanceTableViewController {
     override func tableView(_ tableView: UITableView, didSelectRowAt indexPath: IndexPath) {
         tableView.deselectRow(at: indexPath, animated: true)
+        
+        if indexPath.section == 0 {
+            self.performSegue(withIdentifier: "ToOnSiteSignup", sender: nil)
+            return
+        }
         
         guard let members = Organization.current?.members, indexPath.row < members.count else { return }
         let member = members[indexPath.row]
