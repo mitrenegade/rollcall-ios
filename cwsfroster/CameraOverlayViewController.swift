6f43e82595938ac5bb7961f262d2f3ad8fe47d30
Bobby <bobbyren@gmail.com>
2017-03-13 00:36:57 -0400

    added custom icons; moved didClickButton functionality to CameraOverlayView class

--- cwsfroster/CameraOverlayViewController.swift
+++ cwsfroster/CameraOverlayViewController.swift
@@ -15,8 +15,13 @@ import UIKit
 }
 class CameraOverlayViewController: UIViewController, UIImagePickerControllerDelegate, UINavigationControllerDelegate{
     
-    @IBOutlet var button: UIButton!
+    @IBOutlet var buttonCapture: UIButton!
+    @IBOutlet var buttonDevice: UIButton!
+    @IBOutlet var buttonLibrary: UIButton!
+    @IBOutlet var buttonCancel: UIButton!
+    
     weak var delegate: CameraControlsDelegate?
+    var picker: UIImagePickerController?
     
     override func viewDidLoad() {
         super.viewDidLoad()
@@ -35,6 +40,8 @@ class CameraOverlayViewController: UIViewController, UIImagePickerControllerDele
         picker.allowsEditing = true
         if UIImagePickerController.isSourceTypeAvailable(.camera) {
             picker.sourceType = .camera // use rear camera because the organizer is taking photos
+            picker.cameraCaptureMode = .photo
+            picker.showsCameraControls = false
         }
         else if UIImagePickerController.isSourceTypeAvailable(.photoLibrary) {
             picker.sourceType = .photoLibrary
@@ -42,16 +49,23 @@ class CameraOverlayViewController: UIViewController, UIImagePickerControllerDele
         else {
             picker.sourceType = .savedPhotosAlbum
         }
+
+        // set up buttons to make them white
+        let polaroid = UIImage(imageLiteralResourceName: "polaroid").withRenderingMode(.alwaysTemplate)
+        self.buttonLibrary.setImage(polaroid, for: .normal)
+        self.buttonLibrary.tintColor = UIColor.white
+
+        let flip = UIImage(imageLiteralResourceName: "flip").withRenderingMode(.alwaysTemplate)
+        self.buttonDevice.setImage(flip, for: .normal)
+        self.buttonDevice.tintColor = UIColor.white
+
+        // CameraOverlayViewController handles functionality and overlay, but is never presented. Its picker is presented.
+        let view = self.view as! CameraOverlayView
+        view.controller = self
         
-        // HACK: this controller is only used for the view and to handle photo library
-        // the photo library button does not appear exactly where you want, but always seems to be in the top right corner
         controller.present(picker, animated: true, completion: nil)
-        
-        // add overlayview
-//        let customView:CameraControlsOverlayView = self.view as! CameraControlsOverlayView
-//        customView.frame = picker.view.frame
-        
         picker.cameraOverlayView = self.view
+        self.picker = picker
     }
     
     func imagePickerController(_ picker: UIImagePickerController, didFinishPickingMediaWithInfo info: [String : Any]) {
@@ -66,7 +80,33 @@ class CameraOverlayViewController: UIViewController, UIImagePickerControllerDele
         delegate?.dismissCamera()
     }
     
-    @IBAction func didClickButton(_ sender: AnyObject?) {
-        print("button")
+
+}
+
+class CameraOverlayView: UIView {
+    var controller: CameraOverlayViewController?
+    
+    // this @IBAction must be defined in the custom view because the custom view responds to UI touches, not the controller
+    @IBAction func didClickButton(_ sender: UIButton?) {
+        if sender == controller?.buttonCapture {
+            print("capture")
+            controller?.picker?.takePicture()
+        }
+        else if sender == controller?.buttonDevice {
+            print("device")
+            if controller?.picker?.cameraDevice == .front {
+                controller?.picker?.cameraDevice = .rear
+            }
+            else {
+                controller?.picker?.cameraDevice = .front
+            }
+        }
+        else if sender == controller?.buttonCancel {
+            print("cancel")
+            controller?.delegate?.dismissCamera()
+        }
+        else if sender == controller?.buttonLibrary {
+            print("library")
+        }
     }
 }
