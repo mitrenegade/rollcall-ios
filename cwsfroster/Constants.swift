acfc776fd959f96bb1d17403ae2bbe6d812465ca
bobbyren <bobbyren@gmail.com>
2018-07-24 00:32:17 -0400

    version bump to 1.3.3 (24); testing=false

--- cwsfroster/Constants.swift
+++ cwsfroster/Constants.swift
@@ -10,7 +10,7 @@ import UIKit
 import Foundation
 import Firebase
 
-let TESTING = true
+let TESTING = false
 let OFFLINE_MODE = false
 
 let powerUserPromptDeferDate: String = "powerUserPromptDeferDate"
