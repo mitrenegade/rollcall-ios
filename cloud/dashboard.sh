ace6c53be2d8c6b6e2776d5b8f0f566e075346b8
bobbyren <bobbyren@gmail.com>
2018-12-26 00:22:10 -0500

    added firebase functions; added stripeConnect callback to index

---
+++ cloud/dashboard.sh
@@ -0,0 +1,3 @@
+killall -9 node
+parse-dashboard --appId KAu5pzPvmjrFNdIeaB5zYb2la2Fs2zRi2JyuQZnA --masterKey DevwbKb6kwQwe3PHRDOeTAF4OJZESUymqtk3od4B --serverURL "https://lunr-server.herokuapp.com/parse" &
+ps aux | grep dashboard
