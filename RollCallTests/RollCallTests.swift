02f2664fa300c8c86bfa0d6ddc12aa402cd2440d
Bobby Ren <bobbyren@gmail.com>
2021-09-10 17:09:09 -0700

    Aded RollCallTests target

---
+++ RollCallTests/RollCallTests.swift
@@ -0,0 +1,26 @@
+//
+//  RollCallTests.swift
+//  RollCallTests
+//
+//  Created by Bobby Ren on 9/10/21.
+//  Copyright © 2021 Bobby Ren. All rights reserved.
+//
+
+import XCTest
+@testable import RollCall
+
+class RollCallTests: XCTestCase {
+
+    var userService: UserService!
+
+    override func setUp() {
+    }
+
+    override func tearDown() {
+    }
+
+    func testStandardFeaturesAvailable() {
+
+    }
+
+}
